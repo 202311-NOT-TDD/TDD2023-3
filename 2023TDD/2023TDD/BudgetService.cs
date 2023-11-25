6c35750551c89473d48acdb6ba1cfefe77d81a50
Joey Chen <jay.hatelove@gmail.com>
2023-11-25 21:54:23 +0800

    remove empty if/else block

--- 2023TDD/2023TDD/BudgetService.cs
+++ 2023TDD/2023TDD/BudgetService.cs
@@ -27,21 +27,6 @@ public class Period
         DateTime overlappingStart = Start > budget.FirstDay()
             ? Start
             : budget.FirstDay();
-        if (budget.YearMonth == Start.ToString("yyyyMM"))
-        {
-            // overlappingEnd = budget.LastDay();
-            // overlappingStart = Start;
-        }
-        else if (budget.YearMonth == End.ToString("yyyyMM"))
-        {
-            // overlappingEnd = End;
-            // overlappingStart = budget.FirstDay();
-        }
-        else
-        {
-            // overlappingEnd = budget.LastDay();
-            // overlappingStart = budget.FirstDay();
-        }
 
         return (overlappingEnd - overlappingStart).Days + 1;
     }
